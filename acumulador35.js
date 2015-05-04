2dde3c031f37b3fadc2a212b6c9604ab8408eaec
desafecto <eldesafecto@gmail.com>
2015-05-04 22:53:22 +0200

    caca culo pedo pis

--- acumulador35.js
+++ acumulador35.js
@@ -2,17 +2,17 @@ angular
 .module("miapp", [])
 .controller('miAppCtrl', function(){
 	scope = this;
-	scope.titulo = "Sumador-restador gilipollesco II"
+	scope.titulo = "Sumador-restador gilipollesco II";
 	scope.cantidad = null;
 	scope.acumulado = 0;
 	scope.valor = 0;
 	scope.sumar = function(){
 		scope.acumulado += parseInt(scope.cantidad);
-		scope.cantidad = null;
-	}
+		scope.cantidad = 0;
+	};
 	scope.restar = function(){
 		scope.acumulado -= parseInt(scope.cantidad);
-		scope.cantidad = null;
+		scope.cantidad = 0;
 	};
 
 });
--- studentController.js
+++ studentController.js
@@ -3,16 +3,10 @@ angular
 .controller('studentCtrl', miControlador);
 
 function miControlador() {
-    var mivar = this;
-    mivar.kalumnos= {
-    	uno: "antonio",
-    	dos: "Pedro",
-    	tres: "Maria"
-    };                        	
+    var mivar = this;                       	
     mivar.alumnos = [
 	    {nombre:'antonio', curso: '3ºBUP', telefono: '912345678'},
 	    {nombre:'luis', curso: '5º', telefono: '91234567'},
 	    {nombre:'pedro', curso: '2ºBUP', telefono: '606789012'}
-	    ]
-    mivar.nada ='nadita';
-     };
+	    ];
+};
