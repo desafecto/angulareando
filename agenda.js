cac604c68bdf17af6d97cbb4e2e3f3678e8e2a38
desafecto <eldesafecto@gmail.com>
2015-06-12 00:06:26 +0200

    Ejercicio del MOOC

--- agenda.js
+++ agenda.js
@@ -1,15 +1,17 @@
  function agenda (titulo, inic) {
+  
   var _titulo = titulo;
   var _contenido = inic;
  
   return {
-    titulo: function()                    { return _titulo; },
+    titulo: function(){ return _titulo; },
     meter:  function(nombre, tf) { _contenido[nombre]=tf; },
-    tf:     function(nombre)         { return _contenido[nombre]; },
-    borrar: function(nombre)     { delete _contenido[nombre]; },
-    toJSON: function()              { return JSON.stringify(_contenido);}
+    tf:     function(nombre){ return _contenido[nombre];  },
+    borrar: function(nombre){ delete _contenido[nombre];  },
+    toJSON: function(){ return JSON.stringify(_contenido);}
   }
 }
+
 var amigos = agenda ("Amigos",
              { Pepe: 113278561,
                José: 157845123,
